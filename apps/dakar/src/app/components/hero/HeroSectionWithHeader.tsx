216228106cc28450b4701b5a3b2b56ab4ec387f2
Nicolas Zozol <nzozol@gmail.com>
2024-12-05 11:44:01 +0100

    feat: diagonal hero

--- apps/dakar/src/app/components/hero/HeroSectionWithHeader.tsx
+++ apps/dakar/src/app/components/hero/HeroSectionWithHeader.tsx
@@ -7,14 +7,13 @@ export const HeroSectionWithHeader = () => {
     <section className="relative bg-background-hero bg-cover bg-center w-full">
       <Header />
       <EmptyLine size={2} />
-      {/*<div className="absolute inset-0 bg-blue-900 bg-opacity-40"></div>*/}
       <h1 className="text-white text-4xl mx-10">
         The best guide to 🏄 in Dakar 🌊
       </h1>
       <EmptyLine size={2} />
 
       <div className="relative h-[400px]  w-full ">
-        <div className="absolute  mx-auto h-[400px] w-full">
+        <div className="absolute  mx-auto h-[400px] w-full z-10 ">
           <div className="w-[70%] mx-auto">
             <div
               className="h-[400px] bg-cover bg-center w-full top-40"
@@ -23,8 +22,24 @@ export const HeroSectionWithHeader = () => {
           </div>
         </div>
 
-        <div className="h-[200px] bg-background-hero"></div>
-        <div className="h-[200px] bg-background-body"></div>
+        <div className="relative h-[100px] bg-background-hero"></div>
+        <div className="relative h-[200px] bg-background-hero  w-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="100%"
+            height="200"
+            viewBox="0 0 1024 200"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M0,200 L1024,0 L1024,200  Z"
+              stroke="white"
+              strokeWidth="2"
+              fill="white"
+            />
+          </svg>
+        </div>
+        <div className="h-[100px] bg-background-body"></div>
       </div>
     </section>
   );
